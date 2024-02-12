6c31d0f551632a6052c27c715c0dca95a4c11bf8
m4tiolli <gabrielmatiollif@gmail.com>
2024-02-12 16:39:51 -0300

    modals

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,8 +4,23 @@ import { TbWorld } from "react-icons/tb";
 import "./Header.scss";
 import { useEffect, useState } from "react";
 import classNames from "classnames";
+import { useDisclosure } from "@chakra-ui/react";
+import { Modal, ModalOverlay, ModalContent } from "@chakra-ui/react";
+import { BsFillMoonFill } from "react-icons/bs";
 
 function Header() {
+  const {
+    isOpen: isLang,
+    onOpen: onLang,
+    onClose: closeLang,
+  } = useDisclosure();
+
+  const {
+    isOpen: isDark,
+    onOpen: onDark,
+    onClose: closeDark,
+  } = useDisclosure();
+
   const [words] = useState([<Name direction={"left"} />, <Email />]);
   const [currentIndex, setCurrentIndex] = useState(0);
   const [isHidden, setIsHidden] = useState(false);
@@ -21,6 +36,19 @@ function Header() {
 
     return () => clearInterval(intervalId);
   }, [words]);
+
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleCheckboxChange = () => {
+    setIsChecked(!isChecked);
+    if (!isChecked) {
+      localStorage.setItem("theme", "dark");
+      document.documentElement.classList.add("dark");
+    } else {
+      localStorage.setItem("theme", "light");
+      document.documentElement.classList.remove("dark");
+    }
+  };
 
   return (
     <header className="h-[13%] px-4 shadow-2xl shadow-black flex items-center justify-center relative">
@@ -44,9 +72,71 @@ function Header() {
         </ul>
       </nav>
       <div className="w-1/4 flex items-center justify-end gap-4">
-        <FiMoon className="text-[#1a1a1a] text-3xl hover:opacity-70 cursor-pointer transition-all active:opacity-90" />
-        <TbWorld className="text-[#1a1a1a] text-3xl hover:opacity-70 cursor-pointer transition-all active:opacity-90" />
+        <FiMoon
+          className="text-[#1a1a1a] text-3xl hover:opacity-70 cursor-pointer transition-all active:opacity-90"
+          onClick={onDark}
+        />
+        <TbWorld
+          className="text-[#1a1a1a] text-3xl hover:opacity-70 cursor-pointer transition-all active:opacity-90"
+          onClick={onLang}
+        />
       </div>
+
+      <Modal isOpen={isDark} onClose={closeDark}>
+        <ModalOverlay />
+        <ModalContent borderRadius={1000} width="fit-content">
+          <div className="dark:bg-zinc-900 bg-zinc-300 py-8 flex justify-evenly items-center rounded-md w-[80vw] lg:w-[20vw]">
+            <BsFillMoonFill className="dark:text-zinc-300 text-zinc-900 text-xl" />
+            <p className="dark:text-zinc-300 text-zinc-900">Dark mode</p>
+            <label className="flex cursor-pointer select-none items-center">
+              <div className="relative">
+                <input
+                  type="checkbox"
+                  checked={isChecked}
+                  onChange={handleCheckboxChange}
+                  className="sr-only"
+                />
+                <div
+                  className={`box block h-6 w-10 rounded-full dark:bg-zinc-300 bg-zinc-900`}
+                ></div>
+                <div
+                  className={`absolute left-1 top-1 flex h-4 w-4 items-center justify-center rounded-full transition dark:bg-zinc-900 bg-zinc-300 ${
+                    isChecked ? "translate-x-full" : ""
+                  }`}
+                ></div>
+              </div>
+            </label>
+          </div>
+        </ModalContent>
+      </Modal>
+
+      <Modal isOpen={isLang} onClose={closeLang}>
+        <ModalOverlay />
+        <ModalContent borderRadius={1000} width="fit-content">
+          <div className="dark:bg-zinc-900 bg-zinc-300 py-8 flex justify-evenly items-center rounded-md w-[80vw] lg:w-[20vw]">
+            <BsFillMoonFill className="dark:text-zinc-300 text-zinc-900 text-xl" />
+            <p className="dark:text-zinc-300 text-zinc-900">Dark mode</p>
+            <label className="flex cursor-pointer select-none items-center">
+              <div className="relative">
+                <input
+                  type="checkbox"
+                  checked={isChecked}
+                  onChange={handleCheckboxChange}
+                  className="sr-only"
+                />
+                <div
+                  className={`box block h-6 w-10 rounded-full dark:bg-zinc-300 bg-zinc-900`}
+                ></div>
+                <div
+                  className={`absolute left-1 top-1 flex h-4 w-4 items-center justify-center rounded-full transition dark:bg-zinc-900 bg-zinc-300 ${
+                    isChecked ? "translate-x-full" : ""
+                  }`}
+                ></div>
+              </div>
+            </label>
+          </div>
+        </ModalContent>
+      </Modal>
     </header>
   );
 }
